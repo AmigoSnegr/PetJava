30ecb7b57c9a068b7b5688c72ae5581b95875fdd
Denis Bizhanov <bizdenis3@gmail.com>
2021-03-15 20:13:11 +0100

    Task 1 and 2 done(refresh)

--- src/GenericsPractice/ArrList.java
+++ src/GenericsPractice/ArrList.java
@@ -1,6 +1,11 @@
 package GenericsPractice;
 
 import java.util.List;
+/** Task 1.
+ *  Write a method that swaps two elements of an array (the array can be of any reference type);
+ *  Task 2.
+ *  Write a method that converts an array to an ArrayList;
+ * */
 
 public class ArrList {
     public static void arraySubstituter(int[]arr, int a, int b){
@@ -9,9 +14,9 @@ public class ArrList {
         arr[b] = c;
     }
 
-    public static void arrToList(int[]arr, List list){
-        for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
+    public static void arrToList(int[]arr, List <Integer> list){
+        for (int j : arr) {
+            list.add(j);
         }
     }
 
